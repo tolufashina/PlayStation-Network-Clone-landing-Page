f70ab73d8d70145d6771b5b1229b2882f482f7f8
tolufashina <fashinatoluwa@gmail.com>
2023-09-18 10:24:55 -0700

    third commit

---
+++ build/js/sony.js
@@ -0,0 +1,125 @@
+// let name1 = "My name is Toluwalope is Joseph is";
+// let age = 18;
+// let isMarried = false;
+// let city = null;
+// let country;
+
+// newName = name1.split(" ");
+// console.log(`${newName}`);
+
+
+
+// fash = name1.split(" ");
+
+// console.log(fash);
+
+// fash.forEach(function(value, index){
+//     console.log(`The value is ${value} and index is ${index}`);
+// });
+
+
+// let numbers = [80,37,78,89,100,130,15,83,56,34,29,28,90];
+
+// newNumbers = numbers.filter(function(newNum){
+//     return newNum >= 80;
+// })
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// let re = "Joebaba"
+
+// let je =re.repeat(8);
+
+// newAr = je.split(" ");
+// console.log(newAr);
+
+// let firstNum = 0;
+// let secondNum = 1;
+
+// while (firstNum < 10){
+//     console.log(firstNum);
+//     firstNum = firstNum + secondNum;
+// }
+
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+
+//   for(i = 0; i < countries.length; i++){
+//     console.log(countries[i]);
+//   }
+
+
+//   let j = 10;
+
+//   while(j >= 0){
+//     console.log(`The list of countries are as follows ${countries[j]}`);
+//     j--;
+//   }
+
+
+const hashTag = [ "#", "##","###","####","#####","######",]
+
+// for(i = 0; i < hashTag.length; i++){
+//     console.log(`${hashTag[i]}\n`);
+// }
+
+const newNums = [1,2,3,4,5,6,7,8,9,10];
+
+// for(const num of newNums){
+//     console.log(`${num} * ${num} = ${num * num}`);
+// }
+
+
+// let x = 1;
+
+// while(x <= 100){
+//     if(x % 2 == 0){
+//         console.log(x);
+//     }
+//     x++;
+// }
+
+// let summ = 0;
+// for(g = 0; g <= 100; g++){
+//     summ += g;
+// }
+
+// console.log(summ);
+
+let aryks = [];
+
+// const newArr = [];
+
+// aryks.push(;
+
+console.log(aryks.push(Math.floor(Math.random() * (6-0)+0)));
+console.log(aryks)
+
+// newArr.push(Math.floor(Math.random() * (6-0)+0));
+
